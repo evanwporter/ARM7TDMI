2bd23cf472c6ed1733fb7f8f2484ed29ac82f7d6
Evan Porter <115374841+evanwporter@users.noreply.github.com>
2026-02-11 13:08:51 -0800

    remove 6785

--- v1/remove_test.py
+++ v1/remove_test.py
@@ -2,7 +2,7 @@
 import os
 from struct import unpack_from, pack
 
-TARGET_INDEX = 672
+TARGET_INDEX = 6785
 TARGET_FILE = r".\tests\GameboyAdvanceCPUTests\v1\arm_ldr_str_register_offset.json.bin"
 
 
