d96bb999368064d44f4e7b1da33cabce75175256
Evan Porter <115374841+evanwporter@users.noreply.github.com>
2026-02-11 17:10:29 -0800

    remove 32977

--- v1/remove_test.py
+++ v1/remove_test.py
@@ -2,7 +2,7 @@
 import os
 from struct import unpack_from, pack
 
-TARGET_INDEX = 29630
+TARGET_INDEX = 32977
 TARGET_FILE = r".\tests\GameboyAdvanceCPUTests\v1\arm_ldr_str_register_offset.json.bin"
 
 
